f43f1704867a1f96c49fef8ba90b3435e6de6a8f
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-06-15 22:49:33 +0530

    setup: basic env setup

---
+++ feature-store/infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3"  {
+    bucket = "flashfeat-tfstate-ap-south-1"
+    key = "dev/terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "tfstate-lock"
+    encrypt = true
+  }
+}
