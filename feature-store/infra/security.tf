299931e1664dda6965b8af64b2e49853c30917b3
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-06-20 23:00:31 +0530

    feat(security):added security group

---
+++ feature-store/infra/security.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "flashfeat_enclave_sg" {
+  name        = "flashfeat-enclave-sg"
+  vpc_id      = aws_vpc.flashfeat_dev
+  description = "Allow HTTP/s from given ip(office); vsock (CID 3) local only"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.office_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    project     = "flashfeat"
+    environment = "dev"
+  }
+}
+
+variable "office_cidr" {
+  description = "Public IP/CIDR of office VPN"
+  type        = string
+}
