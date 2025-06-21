51c83da4c03827cc2d63ca3d28da0832c8ab5e88
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-06-21 12:25:20 +0530

    feat(security): fix id error

--- feature-store/infra/security.tf
+++ feature-store/infra/security.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "flashfeat_enclave_sg" {
   name        = "flashfeat-enclave-sg"
-  vpc_id      = aws_vpc.flashfeat_dev
+  vpc_id      = aws_vpc.flashfeat_dev.id
   description = "Allow HTTP/s from given ip(office); vsock (CID 3) local only"
 
   ingress {
